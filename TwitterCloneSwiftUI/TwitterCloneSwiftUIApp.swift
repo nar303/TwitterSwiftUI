62faf6b0cbce5ba9293c3d676e005998a1f7fcdf
Narek <narek.ghukasyan303@gmail.com>
2022-10-03 15:18:49 +0400

    Initial Commit

---
+++ TwitterCloneSwiftUI/TwitterCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneSwiftUIApp.swift
+//  TwitterCloneSwiftUI
+//
+//  Created by Narek Ghukasyan on 03.10.22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
